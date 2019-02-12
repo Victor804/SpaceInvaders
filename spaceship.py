9215a7da61e1d83d232c329ca471b58609997076
Victor804 <victor.vaucel@gmail.com>
2019-02-12 14:19:28 +0100

    init

---
+++ spaceship.py
@@ -0,0 +1,30 @@
+import pygame
+from lxml import etree
+import sys, os
+
+class Spaceship:
+	def __init__(self, directory):
+		self.name = etree.parse(sys.path[0]+"/spaceships"+directory+"/model.xml").xpath("/spaceship/name")[0].text
+		self.
+		self.life = eval(etree.parse(sys.path[0]+"/spaceships"+directory+"/model.xml").xpath("/spaceship/life")[0].text)
+		self.speed = eval(etree.parse(sys.path[0]+"/spaceships"+directory+"/model.xml").xpath("/spaceship/speed")[0].text)
+		self.list_pictures = self._load_pictures(directory)
+
+
+	def _load_pictures(self, directory):
+		"""
+		Entree:	Dossier du vaisseau
+		Sortie: Liste des surfaces des images dans une liste
+		"""
+		list_pictures = list()
+		for file in os.listdir(sys.path[0]+"/spaceships{}/animation".format(directory)):
+			list_pictures.append(pygame.image.load(sys.path[0]+"/spaceships{}/animation/{}".format(directory, file)).convert_alpha())
+		return list_pictures
+
+
+if __name__ == "__main__":
+	spaceship = Spaceship("/simple")
+	print(spaceship.name)
+	print(spaceship.life)
+	print(spaceship.speed)
+	print(spaceship.list_pictures)
